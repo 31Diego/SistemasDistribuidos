7dfb6ccd65c5ed3e0e48b04957eec84b8cf41a89
31Diego <diegomartinezt31@gmail.com>
2025-09-24 09:05:23 +0200

    clase

---
+++ clase/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        String linea = Console.ReadLine();
+        Console.WriteLine(linea.ToUpper());
+    }
+}
