5f0ea56321e2b2d651b27b9b4a34b2fb3d96276f
31Diego <diegomartinezt31@gmail.com>
2025-09-28 16:29:55 +0200

    hola mundo

---
+++ holaMundo/Persona.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+
+namespace holaMundo
+{
+    class Persona
+    {
+        private string _nombre;
+        private int _edad;
+
+
+        public Persona(string nombre, int edad)
+        {
+            _nombre = nombre;
+            _edad = edad;
+        }
+
+
+        public void Saludar()
+        {
+            Console.WriteLine($"Hola, soy {_nombre} y tengo {_edad} años");
+        }
+
+    }
+}
+    
---
+++ holaMundo/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+
+namespace holaMundo
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona("carlos", 23);
+            persona.Saludar();
+        }
+    }
+}
